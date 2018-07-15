08851b51b53d51c5fc67073f266c9f87a2beaa41
tarunsawlani8@gmail.com <tarunsawlani8@gmail.com>
2018-07-15 18:39:55 +0530

    Changes for pdf

--- src/main/java/com/amaropticals/model/ItemModel.java
+++ src/main/java/com/amaropticals/model/ItemModel.java
@@ -2,9 +2,19 @@ package com.amaropticals.model;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+
 public class ItemModel {
 	
 	private int productId;
+	
+	
 	private int buyQuantity;
 	private String productName;
 	private String taskId;
@@ -14,41 +24,53 @@ public class ItemModel {
 	private double unitPrice;
 	private String productDesc;
 	private String code;
+	private String totalCost;
 	
 	
 	private List<LensModel> leftEye;
 	private List<LensModel> rightEye;
 	
+	@XmlElement(name="productid")
 	public int getProductId() {
 		return productId;
 	}
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
+	
+	@XmlElement(name="buyquantity")
 	public int getBuyQuantity() {
 		return buyQuantity;
 	}
 	public void setBuyQuantity(int quantity) {
 		this.buyQuantity = quantity;
 	}
+	
+	@XmlElement(name="productname")
 	public String getProductName() {
 		return productName;
 	}
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+	
+	@XmlElement(name="taskid")
 	public String getTaskId() {
 		return taskId;
 	}
 	public void setTaskId(String taskId) {
 		this.taskId = taskId;
 	}
+	
+	@XmlElement(name="deliverydate")
 	public String getDeliveryDate() {
 		return deliveryDate;
 	}
 	public void setDeliveryDate(String deliveryDate) {
 		this.deliveryDate = deliveryDate;
 	}
+	
+	@XmlElement(name="lensactive")
 	public boolean isLensActive() {
 		return lensActive;
 	}
@@ -61,18 +83,27 @@ public class ItemModel {
 	public void setPurchaseCode(String purchaseCode) {
 		this.purchaseCode = purchaseCode;
 	}
+	
+	@XmlElement(name="unitprice")
 	public double getUnitPrice() {
 		return unitPrice;
 	}
 	public void setUnitPrice(double unitPrice) {
 		this.unitPrice = unitPrice;
 	}
+	
+
+	@XmlElementWrapper(name="lefteyes")
+	@XmlElement(name="lefteye")
 	public List<LensModel> getLeftEye() {
 		return leftEye;
 	}
 	public void setLeftEye(List<LensModel> leftEye) {
 		this.leftEye = leftEye;
 	}
+	
+	@XmlElementWrapper(name="righteyes")
+	@XmlElement(name="righteye")
 	public List<LensModel> getRightEye() {
 		return rightEye;
 	}
@@ -90,6 +121,14 @@ public class ItemModel {
 	}
 	public void setCode(String code) {
 		this.code = code;
+	}
+	
+	@XmlElement(name="totalcost")
+	public String getTotalCost() {
+		return totalCost;
+	}
+	public void setTotalCost(String totalCost) {
+		this.totalCost = totalCost;
 	}
 	
 	
--- src/main/java/com/amaropticals/model/LensModel.java
+++ src/main/java/com/amaropticals/model/LensModel.java
@@ -1,22 +1,30 @@
 package com.amaropticals.model;
 
+import javax.xml.bind.annotation.XmlElement;
+
 public class LensModel {
 	
 	private String sph;
 	private String cyl;
 	private String axial;
+	
+	@XmlElement(name="sph")
 	public String getSph() {
 		return sph;
 	}
 	public void setSph(String sph) {
 		this.sph = sph;
 	}
+	
+	@XmlElement(name="cyl")
 	public String getCyl() {
 		return cyl;
 	}
 	public void setCyl(String cyl) {
 		this.cyl = cyl;
 	}
+	
+	@XmlElement(name="axial")
 	public String getAxial() {
 		return axial;
 	}
